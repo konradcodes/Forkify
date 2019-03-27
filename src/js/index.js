eb852ca55a38a1647b198ce1eb7224fee43aa019
konradcodes <konkordek47@gmail.com>
2019-03-27 16:37:57 +0000

    List Model, View and Controller Implemented

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** //? Global State of the app
@@ -13,6 +14,7 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  * - Liked recipes
  */
 const state = {};
+window.state = state;
 
 //?Search Controller
 const controlSearch = async () => {
@@ -91,6 +93,36 @@ const controlRecipe = async () => {
 
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe));
 
+//? LIST Controller
+
+const controlList = () => {
+  //Create a new list IF there is none yet
+  if (!state.list) state.list = new List();
+
+  //Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+//Handle delete and update list item events
+elements.shopping.addEventListener("click", e => {
+  const id = e.target.closest(".shopping__item").dataset.itemid;
+
+  //Handle the delete button
+  if (e.target.matches(".shopping__delete, .shopping__delete *")) {
+    //Delete from state
+    state.list.deleteItem(id);
+    //Delete from UI
+    listView.deleteItem(id);
+
+    //Handle the count update
+  } else if (e.target.matches(".shopping__count-value")) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
 //Handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
   if (e.target.matches(".btn-decrease, .btn-decrease *")) {
@@ -103,6 +135,8 @@ elements.recipe.addEventListener("click", e => {
     //Increase button is clicked
     state.recipe.updateServings("inc");
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+    controlList();
   }
 });
 
